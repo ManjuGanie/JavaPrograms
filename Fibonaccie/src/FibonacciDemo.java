2b9d780aeeb25eb7c3235b2b9436c2bb4d63165d
acer <acer@Z1-471>
2021-10-08 12:30:18 +0530

    FibonacciPrograms

---
+++ Fibonaccie/src/FibonacciDemo.java
@@ -0,0 +1,14 @@
+
+public class FibonacciDemo {
+	public static void main(String args[]) {
+		int n = 100, t1 = 0, t2 = 1;
+		System.out.println("Upto"+n+",");
+		while(t1<=n)
+		{
+			System.out.println(t1+"+");
+			int sum = t1 + t2;
+			t1 = t2;
+			t2 = sum;
+		}	
+	}
+}
