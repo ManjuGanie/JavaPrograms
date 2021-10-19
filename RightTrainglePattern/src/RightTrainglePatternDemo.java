9189f39ed0103ce1c0d67ecc048b01aa47188adb
acer <acer@Z1-471>
2021-10-19 10:03:00 +0530

    RightAngleTriangle

---
+++ RightTrainglePattern/src/RightTrainglePatternDemo.java
@@ -0,0 +1,20 @@
+
+public class RightTrainglePatternDemo 
+{
+public static void main(String args[]) 
+{
+	int i,j,row=6;
+	
+	for (i=0; i<row; i++)
+	{
+		for (j=0; i<=i; j++)
+		{
+			System.out.println("*");
+			
+		}
+		System.out.println();
+		
+	}	
+		
+}
+}
