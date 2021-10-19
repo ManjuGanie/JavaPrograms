70bf29d52692dd327c4bbc1e9a11cdeecde5d92e
acer <acer@Z1-471>
2021-10-19 08:54:23 +0530

    AsciiValue

---
+++ Ascii/src/AsciiDemo.java
@@ -0,0 +1,17 @@
+
+public class AsciiDemo 
+{
+public static void main (String args[]) 
+{
+char ch = 'e';
+
+int asciiCode= ch;
+
+int asciiValue = (int)ch;
+
+System.out.println("ASCII value of "+ch+" is:"+ asciiCode);
+
+System.out.println("ASCII value of "+ch+" is:"+ asciiValue);
+
+	}
+}
